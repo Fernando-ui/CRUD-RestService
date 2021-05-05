521f0276a51f0896f74d8956e0fbfe9ebce1e8f1
fersan <fst-96@hotmail.com>
2021-05-05 17:48:36 -0500

    Añadiendo verificaciones nombre,password,correo y rol

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,7 +1,7 @@
 const { response, request } = require('express');
 const Usuario = require('../models/usuario');
 const bcrypt = require('bcryptjs');
-const { validationResult } = require('express-validator');
+
 
 
 const usuariosGet =  (req = request, res = response) => {
@@ -17,13 +17,7 @@ const usuariosGet =  (req = request, res = response) => {
 };
 const usuariosPost = async (req = request, res = response) => {
 
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-
-        return res.status(400).json(errors);
     
-    }
-
     const {nombre, correo, password, rol} = req.body;
     const usuario = new Usuario({ nombre, correo, password, rol});
     
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -8,13 +8,18 @@ const { usuariosGet,
         usuarioPut,
         usuarioDelete,
         usuarioPatch } = require('../controllers/usuarios');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 router.get('/',usuariosGet);
 router.post('/', [
 
+    check('nombre','El nombre es obligatorio').not().isEmpty(),
+    check('password','El password es obligatorio y debe de ser una longitud de 6 letras').isLength({min:6}),
     check('correo','El correo no es valido').isEmail(),
+    check('rol','No es un rol permitido').isIn(['ADMIN_ROLE','USER_ROLE']),
+    validarCampos
 
 ],usuariosPost);
 router.put('/:id', usuarioPut);
