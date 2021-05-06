16a4202b0657de976fef4cfc845589c763ed5fdd
fersan <fst-96@hotmail.com>
2021-05-05 19:16:14 -0500

    Añadiendo Verificacion Del Rol

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,7 +1,6 @@
-// Extraemos el put patch get deleted...
 const { Router } = require('express');
 const { check } = require('express-validator');
-
+const Role = require('../models/role');
 // Importamos las instancias de las rutas
 const { usuariosGet,
         usuariosPost,
@@ -18,7 +17,16 @@ router.post('/', [
     check('nombre','El nombre es obligatorio').not().isEmpty(),
     check('password','El password es obligatorio y debe de ser una longitud de 6 letras').isLength({min:6}),
     check('correo','El correo no es valido').isEmail(),
-    check('rol','No es un rol permitido').isIn(['ADMIN_ROLE','USER_ROLE']),
+    // check('rol','No es un rol permitido').isIn(['ADMIN_ROLE','USER_ROLE']),
+    check('rol').custom(async (rol='') => {
+
+        const existeRol = await Role.findOne({rol});
+
+        if(!existeRol){
+
+            throw new Error(`El rol ${rol} no esta registrado en la base de datos`)
+        }
+    }),
     validarCampos
 
 ],usuariosPost);
