c7b4a11e39ff9a6657fd62e143cb2334fc1ffdfb
fersan <fst-96@hotmail.com>
2021-05-05 19:37:07 -0500

    Haciendo codigo mas limpio

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,13 +1,14 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const Role = require('../models/role');
-// Importamos las instancias de las rutas
+
+const { validarCampos } = require('../middlewares/validar-campos');
+const { esRoleValido } = require('../helpers/db-validators');
+
 const { usuariosGet,
         usuariosPost,
         usuarioPut,
         usuarioDelete,
         usuarioPatch } = require('../controllers/usuarios');
-const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
@@ -18,15 +19,7 @@ router.post('/', [
     check('password','El password es obligatorio y debe de ser una longitud de 6 letras').isLength({min:6}),
     check('correo','El correo no es valido').isEmail(),
     // check('rol','No es un rol permitido').isIn(['ADMIN_ROLE','USER_ROLE']),
-    check('rol').custom(async (rol='') => {
-
-        const existeRol = await Role.findOne({rol});
-
-        if(!existeRol){
-
-            throw new Error(`El rol ${rol} no esta registrado en la base de datos`)
-        }
-    }),
+    check('rol').custom(esRoleValido),
     validarCampos
 
 ],usuariosPost);
